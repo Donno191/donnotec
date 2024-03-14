f5f82e62058c1b1a6886455e304cfe68637e1175
Donno <enzer191@yahoo.com>
2024-03-14 20:51:50 +0200

    0.1.3_1

--- start_php_server.sh
+++ start_php_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the desired directory
-cd /home/donno/Documents/Donno_projects/donnotec-main/
+cd /home/donno/Documents/Donno_projects/donnotec/
 
 # Start the PHP server with the specified command
 php -S localhost:8001 -t public_html
